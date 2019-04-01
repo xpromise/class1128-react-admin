15c117125ecc9294455ed67af9907a40fb6c3641
xiongjian <webjsforyou@163.com>
2019-04-01 16:06:13 +0800

    表单校验

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,17 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import Login from './pages/login';
 import Admin from './pages/admin';
 
-// 测试高阶组件
-// import A from './test/login';
-// import B from './test/register';
-
 import './assets/less/reset.less';
 
 export default class App extends Component {
   render () {
     return (
-      <div>
-        {/*<A />*/}
-        {/*<B />*/}
-        <Switch>
-          <Route path="/login" component={Login}/>
-          <Route path="/" component={Admin}/>
-          {/* 为了开发login组件设计的 */}
-          <Redirect to="/login"/>
-        </Switch>
-      </div>
+      <Switch>
+        <Route path="/login" component={Login}/>
+        {/* 为了开发login组件设计的 */}
+        <Redirect to="/login"/>
+        <Route path="/" component={Admin}/>
+      </Switch>
     )
   }
 }
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -11,13 +11,58 @@ import './index.less';
 
 const Item = Form.Item;
 
-export default class Login extends Component {
+@Form.create()
+class Login extends Component {
   
   login = (e) => {
     e.preventDefault();
+    // 校验表单是否通过
+    // 获取一组表单数据
+    // const result = this.props.form.getFieldsValue();
+    // 获取部分表单数据
+    // const result = this.props.form.getFieldsValue(['username']);
+    // 获取单个表单数据
+    // const result = this.props.form.getFieldValue('password');
+    // console.log(result);
+    // 表单校验的方法
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        // 校验成功
+        console.log(values);
+      } else {
+        // 校验失败
+        console.log('****** 表单校验失败 ******');
+        console.log(err);
+        console.log('****** 表单校验失败 ******');
+      }
+    })
+  }
+  
+  /**
+   * 自定义表单的校验规则
+   */
+  validator = (rule, value, callback) => {
+    // console.log(rule, value);
+    const length = value && value.length;
+    const pwdReg = /^[a-zA-Z0-9_]+$/;
+    if (!value) {
+      // callback如果不传参代表校验成功，如果传参代表校验失败，并且会提示错误
+      callback('必须输入密码');
+    } else if (length < 4) {
+      callback('密码必须大于4位');
+    } else if (length > 12) {
+      callback('密码必须小于12位');
+    } else if (!pwdReg.test(value)) {
+      callback('密码必须是英文、数组或下划线组成');
+    } else {
+      // 必须调用callback
+      callback();
+    }
   }
   
   render() {
+    const { getFieldDecorator } = this.props.form;
+    
     return (
       <div className="login">
         <header className="login-header">
@@ -28,10 +73,35 @@ export default class Login extends Component {
           <h3>用户登录</h3>
           <Form onSubmit={this.login} className="login-form">
             <Item>
-              <Input prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>} placeholder="用户名"/>
+              {
+                // getFieldDecorator() 返回值是一个高阶组件
+                // getFieldDecorator()() 返回一个新组件，新组件内部就会给传入的组件定义校验规则
+                // getFieldDecorator(标识名称，配置对象)(组件)
+                // 配置对象 --> 属性名固定的对象
+                getFieldDecorator('username', {
+                  // 表单校验的规则
+                  rules: [
+                    {required: true, whitespace: true, message: '必须输入用户名'},
+                    {min: 4, message: '用户名必须大于4位'},
+                    {max: 12, message: '用户名必须小于12位'},
+                    {pattern: /^[a-zA-Z0-9_]+$/, message: '用户名必须是英文、数组或下划线组成'}
+                  ]
+                })(
+                  <Input prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>} placeholder="用户名"/>
+                )
+              }
             </Item>
             <Item>
-              <Input prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>} type="password" placeholder="密码"/>
+              {
+                getFieldDecorator('password', {
+                  rules: [
+                    // 自定义表单校验规则
+                    {validator: this.validator}
+                  ]
+                })(
+                  <Input prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>} type="password" placeholder="密码"/>
+                )
+              }
             </Item>
             <Item>
               <Button type="primary" htmlType="submit" className="login-form-button">
@@ -44,3 +114,10 @@ export default class Login extends Component {
     )
   }
 }
+
+/*
+  Form.create({ name: 'normal_login' }) 返回值是一个函数（高阶组件）
+  Form.create({ name: 'normal_login' })(Login) 返回值是一个新的组件
+ */
+// export default Form.create()(Login);
+export default Login;
