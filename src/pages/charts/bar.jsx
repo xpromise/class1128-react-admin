559400fcf51cca787c9c59f05cd5415e1fe81156
xiongjian <webjsforyou@163.com>
2019-04-10 15:23:59 +0800

    图表库的使用

--- src/pages/charts/bar.jsx
+++ src/pages/charts/bar.jsx
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import ReactEcharts from 'echarts-for-react';
 
 export default class Bar extends Component {
+
+  getOption = () => {
+    return {
+      title: {
+        text: 'ECharts 入门示例'
+      },
+      tooltip: {},
+      legend: {
+        data:['销量']
+      },
+      xAxis: {
+        data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+      },
+      yAxis: {},
+      series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+      }]
+    };
+  }
+
   render() {
-    return <div>
-      Bar
-    </div>;
+    return <ReactEcharts option={this.getOption()}/>;
   }
 }
